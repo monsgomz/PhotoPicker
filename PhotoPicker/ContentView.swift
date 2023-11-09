3b05b8ccc5ff708c192e8eb57e4b674a0ad89e01
M Montserrat Gomez <gome0145@algonquinlive.com>
2023-11-09 13:15:03 -0500

    Create PhotoPicker project

--- PhotoPicker/ContentView.swift
+++ PhotoPicker/ContentView.swift
@@ -1,24 +1,71 @@
 //
-//  ContentView.swift
-//  PhotoPicker
+//  PhotoPicker.swift
+//  HackingSwfit
 //
-//  Created by Montserrat Gomez on 2023-11-09.
+//  Created by Montserrat Gomez on 2023-11-01.
 //
 
 import SwiftUI
+import PhotosUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+
+struct PhotoPicker: View {
+	
+	@State var avatarImage: UIImage?
+	@State var imageSelection: PhotosPickerItem?
+	
+	var body: some View {
+		VStack{
+			HStack(spacing: 20){
+				Image(uiImage: avatarImage  ?? UIImage(resource: .avatar))
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+					.cornerRadius(20)
+					.frame(width: 100, height: 100, alignment: .center)
+					.overlay(alignment: .bottomTrailing) {
+						
+						/// El PhotoPicker tiene varios argumentos
+						/// matching: .images (puede ser .videos, etc) es como un filtro
+						///se tiene que convertir a un objeto Imagen
+						///
+						PhotosPicker(selection: $imageSelection,
+									 matching: .images,
+									 photoLibrary: .shared()) {
+							Image(systemName: "pencil.circle.fill")
+								.symbolRenderingMode(.multicolor)
+								.font(.system(size: 30))
+								.foregroundColor(.accentColor)
+						}
+					}
+				VStack(alignment: .leading, spacing: 10){
+					Text("Montserrat Gomez")
+						.font(.title)
+					Text("iOS Developer")
+						.font(.caption)
+				}
+				
+			}
+			Spacer()
+		}
+		.padding(10)
+		.onChange(of: imageSelection) { _ , _ in
+			Task{
+				/// Revisar si es null o no la imageSelection
+				/// Data.self es para que lea la misma data de ImageSelection
+				if let imageSelection,
+				   let data = try? await imageSelection.loadTransferable(type: Data.self){
+					if let image = UIImage(data: data){
+						avatarImage = image
+					}
+				}
+				imageSelection = nil
+				
+			}
+		}
+	}
 }
 
 #Preview {
-    ContentView()
+	PhotoPicker()
 }
+

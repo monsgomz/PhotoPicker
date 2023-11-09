49e29a3f64490a7d30febb95e636889360c6f746
M Montserrat Gomez <gome0145@algonquinlive.com>
2023-11-09 13:12:07 -0500

    Initial Commit

---
+++ PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by Montserrat Gomez on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
